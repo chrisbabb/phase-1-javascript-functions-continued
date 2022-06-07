8f28e2a324f5f64ac88ee308ee48e0e20079391e
Christopher Babb <chris@appsoftdev.com>
2022-06-07 15:06:59 -0700

    commit tree 247cccc97c4fbe67108d8d628679583c8d1cfc87

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(doSomething = 'go to the office') {
+    return `This Monday, I will ${doSomething}.`
+}
+
+function wrapAdjective(wrapper = "*") {
+    return function theAdj(myAdj) {
+        return `You are ${wrapper}${myAdj}${wrapper}!`
+    }
+}
